283d204797d9f6ffd203753fa52faac35ba2959d
Alexander Verbeek <sanderman@gmail.com>
2018-01-26 21:06:49 +0100

    PlayerCommand stuff.

---
+++ Rovers/Assets/Scripts/ActionType.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ActionType
+{
+    Forward,
+    Reverse,
+    RotateLeft,
+    RotateRight,
+    Shoot,
+    RotateTurretLeft,
+    RotateTurretRight
+}
