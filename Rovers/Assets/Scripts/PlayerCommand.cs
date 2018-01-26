f92b676ecbd1c786906514bf33c824e9de53f921
Alexander Verbeek <sanderman@gmail.com>
2018-01-26 21:19:19 +0100

    Add PlayerId and RoverId to PlayerCommand

--- Rovers/Assets/Scripts/PlayerCommand.cs
+++ Rovers/Assets/Scripts/PlayerCommand.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerCommand
 {
+    public int PlayerId { get; private set; }
+    public int RoverId { get; private set; }
     public ActionType Action { get; private set; }
     public float SentTime { get; private set; }
 
-    public PlayerCommand(ActionType type, float sentTime)
+    public PlayerCommand(int playerId, int roverId, ActionType type, float sentTime)
     {
+        PlayerId = playerId;
+        RoverId = roverId;
         Action = type;
         SentTime = sentTime;
     }
