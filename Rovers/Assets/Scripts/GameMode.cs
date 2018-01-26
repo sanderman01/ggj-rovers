ff5c8016ca9ab00a71b3f8e74e5f4f5e20b014fc
Alexander Verbeek <sanderman@gmail.com>
2018-01-26 21:47:31 +0100

    Basic code structure.

--- Rovers/Assets/Scripts/GameMode.cs
+++ Rovers/Assets/Scripts/GameMode.cs
@@ -4,4 +4,17 @@ using UnityEngine;
 
 public class GameMode : MonoBehaviour
 {
+    private List<Player> players;
+    private List<Rover> rovers;
+    private SortedList<float, PlayerCommand> commandQueue;
+
+    private void Update()
+    {
+        // For each player, get command input, if any, and put it into the command queue.
+
+        // If it's time to execute commands, then:
+        // For each command in queue where (sentTime + currentDelay) < currentTime
+        // And the rover is not already executing a command
+        //     Then give the command to the rover.
+    }
 }
--- Rovers/Assets/Scripts/Rover.cs
+++ Rovers/Assets/Scripts/Rover.cs
@@ -4,4 +4,44 @@ using UnityEngine;
 
 public class Rover : MonoBehaviour
 {
+    public int PlayerId;
+    public int RoverId;
+
+    public bool IsExecutingCommand { get; private set; }
+
+    public void Initialise(int playerId, int roverId)
+    {
+        PlayerId = playerId;
+        RoverId = roverId;
+    }
+
+    public void ExecuteCommand(PlayerCommand command)
+    {
+        switch (command.Action)
+        {
+            case ActionType.Forward:
+                // Start coroutine or other process
+                break;
+            case ActionType.Reverse:
+                // Start coroutine or other process
+                break;
+            case ActionType.RotateLeft:
+                // Start coroutine or other process
+                break;
+            case ActionType.RotateRight:
+                // Start coroutine or other process
+                break;
+            case ActionType.Shoot:
+                // Start coroutine or other process
+                break;
+            case ActionType.RotateTurretLeft:
+                // Start coroutine or other process
+                break;
+            case ActionType.RotateTurretRight:
+                // Start coroutine or other process
+                break;
+            default:
+                throw new System.NotImplementedException();
+        }
+    }
 }
