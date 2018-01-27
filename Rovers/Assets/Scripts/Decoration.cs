a93f1194de23027800b319e6257cacf719c50738
Alexander Verbeek <sanderman@gmail.com>
2018-01-27 22:09:58 +0100

    Reduced size of map. One square is now one unit/meter wide. Added obstacles.

---
+++ Rovers/Assets/Scripts/Decoration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Decoration : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private Sprite[] sprites;
+
+    private void Awake()
+    {
+        if(!Application.isPlaying)
+        {
+            SelectRandomSprite();
+            SelectRandomRotation();
+        }
+    }
+
+    public void SelectRandomSprite()
+    {
+        int index = Random.Range(0, sprites.Length);
+        spriteRenderer.sprite = sprites[index];
+    }
+
+    public void SelectRandomRotation()
+    {
+        float angle = Random.value * 360f;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
---
+++ Rovers/Assets/Scripts/RandomObstacles.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Spawns an object in random positions.
+public class RandomObstacles : MonoBehaviour
+{
+    [SerializeField]
+    private int objectCount;
+
+    [SerializeField]
+    private Decoration prefab;
+
+    [SerializeField]
+    private Rect area;
+
+    // Use this for initialization
+    void Start()
+    {
+        int xMin = Mathf.FloorToInt(area.xMin);
+        int yMin = Mathf.FloorToInt(area.yMin);
+        int xMax = Mathf.FloorToInt(area.xMax);
+        int yMax = Mathf.FloorToInt(area.yMax);
+
+        for (int i = 0; i < objectCount; i++)
+        {
+            int x = Random.Range(xMin, xMax);
+            int y = Random.Range(yMin, yMax);
+            Vector2 pos = new Vector2(x, y);
+
+            Decoration obj = Instantiate(prefab, transform);
+            obj.transform.position = new Vector2(x, y);
+
+            obj.SelectRandomRotation();
+            obj.SelectRandomSprite();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(area.center, area.size);
+    }
+}
