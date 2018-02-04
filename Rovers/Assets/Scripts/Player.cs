20552e613f2361b5bb7f79fd622b87ebdfa57ff9
Alexander Verbeek <sanderman@gmail.com>
2018-02-04 22:31:02 +0100

    Add keyboard input. Currently hardcoded to Arrow keys and WASD.

--- Rovers/Assets/Scripts/Player.cs
+++ Rovers/Assets/Scripts/Player.cs
@@ -28,26 +28,66 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        bool forward = false;
+        bool left = false;
+        bool right = false;
+        bool shoot = false;
+        bool reverse = false;
+
+        // Key input
+        KeyCode keyForward = KeyCode.UpArrow;
+        KeyCode keyLeft = KeyCode.LeftArrow;
+        KeyCode keyRight = KeyCode.RightArrow;
+        KeyCode keyShoot = KeyCode.DownArrow;
+
+        switch (PlayerId)
+        {
+            case 0:
+                keyForward = KeyCode.W;
+                keyLeft = KeyCode.A;
+                keyRight = KeyCode.D;
+                keyShoot = KeyCode.S;
+                break;
+            case 1:
+                keyForward = KeyCode.UpArrow;
+                keyLeft = KeyCode.LeftArrow;
+                keyRight = KeyCode.RightArrow;
+                keyShoot = KeyCode.DownArrow;
+                break;
+        }
+
+        forward |= Input.GetKeyDown(keyForward);
+        right |= Input.GetKeyDown(keyRight);
+        left |= Input.GetKeyDown(keyLeft);
+        shoot |= Input.GetKeyDown(keyShoot);
+
+        // Gamepad input
         GamePad.Index controllerIndex = (GamePad.Index)(PlayerId + 1);
         Vector2 leftStick = GamePad.GetAxis(GamePad.Axis.LeftStick, controllerIndex);
-        bool shoot = GamePad.GetButton(GamePad.Button.A, controllerIndex)
+        shoot |= GamePad.GetButton(GamePad.Button.A, controllerIndex)
                   || GamePad.GetTrigger(GamePad.Trigger.LeftTrigger, controllerIndex) > deadZone
                   || GamePad.GetTrigger(GamePad.Trigger.LeftTrigger, controllerIndex) > deadZone;
 
+        forward |= leftStick.y > 0 + deadZone;
+        reverse |= leftStick.y < 0 - deadZone;
+        right |= leftStick.x > 0 + deadZone;
+        left |= leftStick.x < 0 - deadZone;
+
+        // Translate to commands
         ActionType newInput = ActionType.None;
 
-        if (leftStick.y > 0 + deadZone)
+        if (forward)
             newInput = ActionType.Forward;
-        else if (leftStick.y < 0 - deadZone)
+        else if (reverse)  
             newInput = ActionType.None; // This disables reverse for now.
-        else if (leftStick.x > 0 + deadZone)
+        else if (right)
             newInput = ActionType.RotateRight;
-        else if (leftStick.x < 0 - deadZone)
+        else if (left)
             newInput = ActionType.RotateLeft;
         else if (shoot)
             newInput = ActionType.Shoot;
 
-        if(newInput != prevInput && newInput != ActionType.None)
+        if (newInput != prevInput && newInput != ActionType.None)
         {
             inputQueue.Enqueue(newInput);
         }
